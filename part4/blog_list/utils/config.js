af1ddc0ce2a956b4c9d22238f2d2ff0e94bb7b64
VM_Ubuntu1 <g1131786239@gmail.com>
2022-06-26 00:24:02 +0300

    part5

--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -5,9 +5,7 @@ const PORT = process.env.PORT
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
-const SECRET = process.env.NODE_ENV === 'test'
-  ? 'test'
-  : process.env.SECRET
+const SECRET = process.env.SECRET
 
 module.exports = {
   MONGODB_URI,
